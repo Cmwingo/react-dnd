44c5f6f63f36e728b01f8837f71268eb8a35cdf1
Wingo <cmwingo@gmail.com>
2017-07-18 13:44:30 -0700

    Correctly retrieve and display classes from API

--- app/containers/app.container.js
+++ app/containers/app.container.js
@@ -5,28 +5,45 @@ class AppContainer extends React.Component {
 
   constructor(props) {
     super(props);
-
     this.state = {
-
+      classes: []
     };
+    this.getClasses = this.getClasses.bind(this);
   }
 
   getClasses() {
-    Axios.get(`api call here`)
+    var classes;
+    console.log("getting classes");
+    Axios.get(`http://dnd5eapi.co/api/classes`)
       .then(response => {
         console.log(response);
+        this.setState({classes: response.data.results});
       })
       .catch(error => {
         console.log(error);
       });
   }
 
-  render() {
-    return (
-
-    );
+  componentDidUpdate() {
+    console.log('Classes: ' + this.state.classes);
+    this.refs.response.append = 'updated';
   }
 
+  render() {
+    return (
+      <div id="char-sheet">
+        <div id="response" ref='response'>
+          <ul>
+            {this.state.classes.map(function(charClass,i) {
+              console.log("i" + i);
+              return <li key={i}>{charClass.name}</li>;
+            })}
+          </ul>
+        </div>
+        <button onClick={this.getClasses}>Get Classes</button>
+      </div>
+    );
+  }
 }
 
 export default AppContainer
