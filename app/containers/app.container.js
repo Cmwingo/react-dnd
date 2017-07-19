8a5446434d2759aa8068768785709ad1bdf566ad
Wingo <cmwingo@gmail.com>
2017-07-19 13:47:07 -0700

    Create a select dropdown to choose character class that registers changes by the user

--- app/containers/app.container.js
+++ app/containers/app.container.js
@@ -41,9 +41,56 @@ class AppContainer extends React.Component {
           </ul>
         </div>
         <button onClick={this.getClasses}>Get Classes</button>
+        <CharCreater />
       </div>
     );
   }
 }
+
+//Character Creation Class
+
+class CharCreater extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      classes: [],
+      value: 'barbarian'
+    };
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(e) {
+    this.setState({value: e.target.value});
+  }
+
+  componentDidMount() {
+    Axios.get('http://dnd5eapi.co/api/classes')
+    .then(response => {
+      console.log(response);
+      this.setState({classes: response.data.results});
+    })
+    .catch(error => {
+      console.log(error);
+    });
+  }
+  render() {
+    return (
+      <div>
+        <h1>Char Creater Component Works!</h1>
+        <label>
+          Select your <span>c</span>lass
+          <select value={this.state.value} onChange={this.handleChange}>
+            {this.state.classes.map(function(charClass,i) {
+              return <option value={charClass.name} key={i}>{charClass.name}</option>;
+            })}
+          </select>
+        </label>
+        <p>You have selected {this.state.value}</p>
+      </div>
+    );
+  }
+}
+
 
 export default AppContainer
